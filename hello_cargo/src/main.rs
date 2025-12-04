b3552dc1354e12e78da242d503728a985cae32eb
sgc93 <smachewgedefaw@gmail.com>
2025-12-05 01:30:05 +0300

    formatting: testing fmt::Display

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,8 +1,9 @@
 fn who_am_i() -> String{
-    return "I am Rustacean".to_string();
+    return "I am Rustacean.".to_string();
 }
 
 fn main() {
     println!("Hello, world!");
-    println!("{}", (who_am_i()));
+    println!("My name is {0}, {1} {0}.", "G", "Smex");
+    println!("{me}", me = (who_am_i()));
 }
