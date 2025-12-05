7f7ae127f8404ef3cb37bc2adecba779954e18be
sgc93 <smachewgedefaw@gmail.com>
2025-12-05 21:53:57 +0300

    add: continue re-entering value instead of exiting for non-numeric value entry

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Please input your guess.");
+        let mut msg = "Please input your guess.".to_string();
+        println!("{msg}");
 
         let mut guess = String::new();
 
@@ -17,16 +18,25 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                msg = "Please enter number!".to_string();
+                continue;
+            },
+        };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Your guess is too small!"),
             Ordering::Greater => println!("Your guess is too big!"),
-            Ordering::Equal => println!("Your guess is correct, You win!"),
+            Ordering::Equal => {
+                println!("Your guess is correct, You win!");
+                break;
+            }
         }
 
-        println!("The secret number is {secret_number}");
+        // println!("The secret number is {secret_number}");
     }
 }
