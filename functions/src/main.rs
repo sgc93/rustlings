2d36a36cda1fef270976514a667da36c354ebab9
sgc93 <smachewgedefaw@gmail.com>
2025-12-08 02:24:31 +0300

    update: functions calling, parameters, arguments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
+fn display_data(name: &str, initial: char){
+    print!("Your name is {name}, so your initial is {initial}.");
+}
+
+fn get_initials(name: &str) -> char {
+    let mut chars = name.chars();
+
+    chars.next().unwrap()
+}
+
+fn get_name() -> String {
+    let mut name = String::new();
+    
+    println!("Please enter your full name");
+    io::stdin().read_line(&mut name).expect("Failed to read your name");
+
+    name
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting ...");
+    let name = get_name();
+    let initial = get_initials(&name);
+    display_data(&name, initial);
 }
